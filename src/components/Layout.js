b8871aeca96263ce4f6eead996c349b3f9c19466
Yoon Lee <himynameisyoon@gmail.com>
2019-07-05 20:58:12 -0700

    add logo to navbar

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,27 +1,44 @@
 import React from "react";
-import { Link } from "@reach/router";
+import { Link, Location } from "@reach/router";
 
 import { LINKS } from "../data/links";
+import logo from "../images/dawgy-inverse.svg";
 
 function Layout({ children }) {
   return (
-    <div className="layout">
-      <nav>
-        {Object.keys(LINKS).map(linkKey => (
-          <Link
-            key={linkKey}
-            getProps={({ isCurrent }) =>
-              isCurrent ? { className: "active" } : null
-            }
-            to={LINKS[linkKey].link}
-          >
-            <div className="iconContainer">icon</div>
-            {LINKS[linkKey].text}
-          </Link>
-        ))}
-      </nav>
-      <main>{children}</main>
-    </div>
+    <Location>
+      {({ location }) => {
+        const isRootPath = location.pathname === "/" ? true : false;
+
+        return (
+          <div className="layout">
+            <nav>
+              <img src={logo} alt="Dawgy logo" />
+              {Object.keys(LINKS).map(linkKey => (
+                <Link
+                  key={linkKey}
+                  className={`${isRootPath ? "root" : ""}`}
+                  getProps={({ isCurrent }) =>
+                    isCurrent ? { className: "active" } : null
+                  }
+                  onClick={e => {
+                    if (isRootPath) {
+                      e.preventDefault();
+                    }
+                  }}
+                  to={LINKS[linkKey].link}
+                  disabled={isRootPath}
+                >
+                  <div className="iconContainer">icon</div>
+                  {LINKS[linkKey].text}
+                </Link>
+              ))}
+            </nav>
+            <main>{children}</main>
+          </div>
+        );
+      }}
+    </Location>
   );
 }
 
--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -2,7 +2,8 @@ import React, { useContext } from "react";
 import { navigate } from "@reach/router";
 
 import UserContext from "../contexts/UserContext";
-import logo from "../images/dawgy.svg";
+
+import Layout from "./Layout";
 
 import { INPUTS } from "../data/inputHelpers";
 
@@ -10,100 +11,102 @@ function StartScreen() {
   const [user, setUser] = useContext(UserContext);
 
   return (
-    <div id="startScreen">
-      <div className="hero">
-        <img src={logo} alt="Dawgy logo" />
-      </div>
+    <Layout>
+      <div id="startScreen">
+        <div className="maxWidth">
+          <form
+            onSubmit={e => {
+              e.preventDefault();
+            }}
+          >
+            <div
+              className="profilePhoto"
+              style={{ backgroundImage: `url(${user.photo})` }}
+            />
 
-      <div className="maxWidth">
-        <form
-          onSubmit={e => {
-            e.preventDefault();
-          }}
-        >
-          <div
-            className="profilePhoto"
-            style={{ backgroundImage: `url(${user.photo})` }}
-          />
-
-          <div className="formFields">
-            <h2>Create Profile</h2>
-            <div className="genderAndBreedRow">
-              <label id="selectGender">
-                <h3>Gender</h3>
-                <div>
-                  <button
-                    id="buttonMale"
-                    className={`${user.gender === "M" ? "active" : ""}`}
-                    onClick={() => setUser(user => ({ ...user, gender: "M" }))}
-                  >
-                    M
-                  </button>
-                  <button
-                    id="buttonFemale"
-                    className={`${user.gender === "F" ? "active" : ""}`}
-                    onClick={() => setUser(user => ({ ...user, gender: "F" }))}
-                  >
-                    F
-                  </button>
-                </div>
-              </label>
-
-              <label id="inputBreed">
-                <h3>Breed</h3>
-                <input value={user.breed} readOnly disabled />
-              </label>
-            </div>
-
-            <div className="bioRow">
-              <label>
-                <h3>Bio</h3>
-                <div>
-                  <textarea value={user.bio} readOnly disabled rows={3} />
-                  <div id="textareaInputs">
-                    {Object.keys(INPUTS).map(inputKey => {
-                      const { text } = INPUTS[inputKey];
-
-                      return (
-                        <button
-                          key={inputKey}
-                          onClick={() => {
-                            setUser(user => ({
-                              ...user,
-                              bio: `${user.bio} ${text}`
-                            }));
-                          }}
-                        >
-                          {text}
-                        </button>
-                      );
-                    })}
+            <div className="formFields">
+              <h2>Create Profile</h2>
+              <div className="genderAndBreedRow">
+                <label id="selectGender">
+                  <h3>Gender</h3>
+                  <div>
+                    <button
+                      id="buttonMale"
+                      className={`${user.gender === "M" ? "active" : ""}`}
+                      onClick={() =>
+                        setUser(user => ({ ...user, gender: "M" }))
+                      }
+                    >
+                      M
+                    </button>
+                    <button
+                      id="buttonFemale"
+                      className={`${user.gender === "F" ? "active" : ""}`}
+                      onClick={() =>
+                        setUser(user => ({ ...user, gender: "F" }))
+                      }
+                    >
+                      F
+                    </button>
                   </div>
-                </div>
-              </label>
+                </label>
+
+                <label id="inputBreed">
+                  <h3>Breed</h3>
+                  <input value={user.breed} readOnly disabled />
+                </label>
+              </div>
+
+              <div className="bioRow">
+                <label>
+                  <h3>Bio</h3>
+                  <div>
+                    <textarea value={user.bio} readOnly disabled rows={3} />
+                    <div id="textareaInputs">
+                      {Object.keys(INPUTS).map(inputKey => {
+                        const { text } = INPUTS[inputKey];
+
+                        return (
+                          <button
+                            key={inputKey}
+                            onClick={() => {
+                              setUser(user => ({
+                                ...user,
+                                bio: `${user.bio} ${text}`
+                              }));
+                            }}
+                          >
+                            {text}
+                          </button>
+                        );
+                      })}
+                    </div>
+                  </div>
+                </label>
+              </div>
+
+              <div>
+                <button
+                  id="buttonCreateProfile"
+                  onClick={() => {
+                    let isValid = true;
+
+                    if (isValid) {
+                      setUser(user => ({ ...user, created: true }));
+                      navigate("/explore");
+                    }
+                  }}
+                >
+                  Create Profile
+                </button>
+              </div>
             </div>
+          </form>
 
-            <div>
-              <button
-                id="buttonCreateProfile"
-                onClick={() => {
-                  let isValid = true;
-
-                  if (isValid) {
-                    setUser(user => ({ ...user, created: true }));
-                    navigate("/explore");
-                  }
-                }}
-              >
-                Create Profile
-              </button>
-            </div>
-          </div>
-        </form>
-
-        {/* <div>Platinum</div> */}
+          {/* <div>Platinum</div> */}
+        </div>
       </div>
-    </div>
+    </Layout>
   );
 }
 
